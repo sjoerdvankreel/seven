8c2a68866240da3aa55904440f710658fc08d5ec
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-08 17:28:39 +0200

    31

--- src/vst/svn/vst/seven_controller.cpp
+++ src/vst/svn/vst/seven_controller.cpp
@@ -3,6 +3,7 @@
 #include <svn/param/synth_params.hpp>
 #include <base/source/fstreamer.h>
 #include <pluginterfaces/base/ibstream.h>
+#include <cstring>
 
 using namespace VSTGUI;
 using namespace Steinberg;
@@ -19,40 +20,33 @@ SevenController::initialize(FUnknown* context)
 {
 	tresult result = EditController::initialize(context);
 	if(result != kResultTrue) return result;
-  parameters.addParameter(STR16(svn::synth_params::unit1_gain.description), STR16 ("dB"), 0, .5,
-		                         Vst::ParameterInfo::kCanAutomate, HelloWorldParams::kParamVolId, 0,
-		                         STR16 ("Param1"));
+  parameters.addParameter(
+    svn::synth_params::unit1_gain.long_name,
+    svn::synth_params::unit1_gain.info.unit, 0,
+    svn::synth_params::unit1_gain.info.default_value,
+    ParameterInfo::kCanAutomate, 
+    svn::synth_params::unit1_gain.id, 0,
+		svn::synth_params::unit1_gain.info.short_name);
 	return kResultTrue;
 }
 
-//------------------------------------------------------------------------
-IPlugView* PLUGIN_API PlugController::createView (const char* name)
+tresult PLUGIN_API 
+SevenController::setComponentState(IBStream* state)
 {
-	// someone wants my editor
-	if (name && strcmp (name, "editor") == 0)
-	{
-		auto* view = new VST3Editor (this, "view", "plug.uidesc");
-		return view;
-	}
-	return nullptr;
+  float value;
+  if(state == nullptr) return kResultFalse;
+	IBStreamer streamer(state, kLittleEndian);
+
+	if(!streamer.readFloat(value)) return kResultFalse;
+	setParamNormalized(svn::param_id::unit1_gain, value);
+	return kResultOk;
 }
 
-//------------------------------------------------------------------------
-tresult PLUGIN_API PlugController::setComponentState (IBStream* state)
+IPlugView* PLUGIN_API
+SevenController::createView(char const* name)
 {
-	// we receive the current state of the component (processor part)
-	// we read our parameters and bypass value...
-	if (!state)
-		return kResultFalse;
-
-	IBStreamer streamer (state, kLittleEndian);
-
-	float savedParam1 = 0.f;
-	if (streamer.readFloat (savedParam1) == false)
-		return kResultFalse;
-	setParamNormalized (HelloWorldParams::kParamVolId, savedParam1);
-
-	return kResultOk;
+  if (name == nullptr || std::strcmp(name, "editor") != 0) return nullptr;
+  return new VST3Editor(this, "view", "seven.uidesc");
 }
 
 } // namespace Svn::Vst
