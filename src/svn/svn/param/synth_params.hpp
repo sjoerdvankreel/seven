9f78e43af5f814db9b8d54f26559129cff59c12b
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-08 17:49:59 +0200

    34

--- src/svn/svn/param/synth_params.hpp
+++ src/svn/svn/param/synth_params.hpp
@@ -15,5 +15,12 @@ inline synth_param unit1_panning { param_id::unit1_panning, L"Unit 1 panning", u
 inline synth_param unit2_gain { param_id::unit2_gain, L"Unit 2 gain", unit_gain_info };
 inline synth_param unit2_panning { param_id::unit2_panning, L"Unit 2 panning", unit_panning_info };
 
+inline synth_param all[] = {
+  unit1_gain,
+  unit1_panning,
+  unit2_gain,
+  unit2_panning
+};
+
 } // namespace svn::synth_params
 #endif // SVN_PARAM_SYNTH_PARAMS_HPP
