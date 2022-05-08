f4a0c8dd229e8fa2e938a611e33f29d77084bf36
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-08 17:08:35 +0200

    TEMP? 29

--- src/vst/svn/vst/seven_controller.cpp
+++ src/vst/svn/vst/seven_controller.cpp
@@ -1,64 +1,27 @@
-//------------------------------------------------------------------------
-// Project     : VST SDK
-//
-// Category    : Examples
-// Filename    : plugcontroller.cpp
-// Created by  : Steinberg, 01/2018
-// Description : HelloWorld Example for VST 3
-//
-//-----------------------------------------------------------------------------
-// LICENSE
-// (c) 2021, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-// 
-//   * Redistributions of source code must retain the above copyright notice, 
-//     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation 
-//     and/or other materials provided with the distribution.
-//   * Neither the name of the Steinberg Media Technologies nor the names of its
-//     contributors may be used to endorse or promote products derived from this 
-//     software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
-
-#include "plugcontroller.h"
-#include "plugids.h"
-
-#include "base/source/fstreamer.h"
-#include "pluginterfaces/base/ibstream.h"
+#include <svn/vst/seven_vst.hpp>
+#include <svn/vst/seven_controller.hpp>
+#include <svn/param/synth_params.hpp>
+#include <base/source/fstreamer.h>
+#include <pluginterfaces/base/ibstream.h>
 
 using namespace VSTGUI;
+using namespace Steinberg;
+using namespace Steinberg::Vst;
 
-namespace Steinberg {   
-namespace HelloWorld {
-  static FUnknown* create_instance(void*)
-  {
-    return (Vst::IEditController*)new PlugController();
-  }
-//-----------------------------------------------------------------------------
-tresult PLUGIN_API PlugController::initialize (FUnknown* context)
+namespace Svn::Vst {
+
+FUnknown* 
+SevenController::createInstance(void* context)
+{ return static_cast<IEditController*>(new SevenController); }
+
+tresult PLUGIN_API 
+SevenController::initialize(FUnknown* context)
 {
-	tresult result = EditController::initialize (context);
-	if (result == kResultTrue)
-	{
-		//---Create Parameters------------
-		parameters.addParameter (STR16 ("Parameter 1"), STR16 ("dB"), 0, .5,
+	tresult result = EditController::initialize(context);
+	if(result != kResultTrue) return result;
+  parameters.addParameter(STR16(svn::synth_params::unit1_gain.description), STR16 ("dB"), 0, .5,
 		                         Vst::ParameterInfo::kCanAutomate, HelloWorldParams::kParamVolId, 0,
 		                         STR16 ("Param1"));
-	}
 	return kResultTrue;
 }
 
@@ -92,6 +55,4 @@ tresult PLUGIN_API PlugController::setComponentState (IBStream* state)
 	return kResultOk;
 }
 
-//------------------------------------------------------------------------
-} // namespace
-} // namespace Steinberg
+} // namespace Svn::Vst
