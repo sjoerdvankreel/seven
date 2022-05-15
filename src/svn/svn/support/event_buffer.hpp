40878057e96209a230a0690bc9a75d6055b9cb1b
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-15 13:24:23 +0200

    74

---
+++ src/svn/svn/support/event_buffer.hpp
@@ -0,0 +1,32 @@
+#ifndef SVN_SUPPORT_EVENT_BUFFER_HPP
+#define SVN_SUPPORT_EVENT_BUFFER_HPP
+
+#include <svn/topology/param_id.hpp>
+#include <cstdint>
+
+namespace svn {
+
+inline std::int32_t constexpr note_off = -1;
+
+struct automation_buffer
+{
+  param_id param;
+  float const* samples;
+};
+
+struct note_buffer
+{
+  std::int32_t count;
+  std::int32_t const* midi;
+  std::int32_t const* indices;
+};
+
+struct event_buffer
+{
+  note_buffer notes;
+  std::int32_t automation_count;
+  automation_buffer const* automation;
+};
+
+} // namespace svn
+#endif // SVN_SUPPORT_EVENT_BUFFER_HPP
