f7eae7292057afa4182723df6583575936367794
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-19 20:15:51 +0200

    85

---
+++ src/svn/svn/support/event_buffer.hpp
@@ -0,0 +1,25 @@
+#ifndef SVN_SUPPORT_INPUT_BUFFER_HPP
+#define SVN_SUPPORT_INPUT_BUFFER_HPP
+#include <cstdint>
+
+namespace svn {
+
+inline std::int32_t constexpr note_off = -1;
+
+struct note_event
+{
+  std::int32_t midi;
+  std::int32_t sample_index;
+};
+
+struct input_buffer
+{
+  float sample_rate;
+  note_event const* notes;
+  std::int32_t note_count;
+  std::int32_t sample_count;
+  void const* const* automation;
+};
+
+} // namespace svn
+#endif // SVN_SUPPORT_INPUT_BUFFER_HPP
