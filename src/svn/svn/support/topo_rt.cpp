22ce072123b40b496e1598d925f221c6b859ecf4
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-19 23:08:30 +0200

    92

--- src/svn/svn/support/topo_rt.cpp
+++ src/svn/svn/support/topo_rt.cpp
@@ -5,12 +5,11 @@
 namespace svn {
 
 std::int32_t synth_part_count;
-synth_part const* synth_parts;
-std::int32_t const* synth_part_bounds;
-
 std::int32_t synth_param_count;
+
+synth_part const* synth_parts;
 synth_param const* synth_params;
-std::int32_t const* synth_param_bounds;
+std::int32_t const* const* synth_bounds;
 
 namespace {
 
@@ -19,35 +18,36 @@ static init_t init_;
 
 static std::vector<synth_part> synth_parts_;
 static std::vector<synth_param> synth_params_;
-
-static std::vector<std::int32_t> synth_part_bounds_;
-static std::vector<std::int32_t> synth_param_bounds_;
+static std::vector<std::int32_t const*> synth_bounds_;
+static std::vector<std::vector<std::int32_t>> param_bounds_;
 
 init_t::
 init_t()
 {
-  std::int32_t prev_type = -1;
+  std::int32_t param_index = 0;
+  for (std::int32_t t = 0; t < part_type::count; t++)
+  {
+    param_bounds_.push_back(std::vector<std::int32_t>());
+    for (std::int32_t i = 0; i < part_infos[t].count; i++)
+    {
+      param_bounds_[t].push_back(param_index);
+      param_index += part_infos[t].param_count;
+    }
+    synth_bounds_.push_back(param_bounds_[t].data());
+  }
+  synth_bounds = synth_bounds_.data();
+
   std::int32_t part_index = 0;
   for(std::int32_t t = 0; t < part_type::count; t++)
-  {
-    if(t != prev_type)
-      synth_part_bounds_.push_back(part_index);
     for(std::int32_t i = 0; i < part_infos[t].count; i++)
       synth_parts_.push_back({ part_index++, &part_infos[t] });
-  }
   synth_parts = synth_parts_.data();
-  synth_part_bounds = synth_part_bounds_.data();
   synth_part_count = static_cast<std::int32_t>(synth_parts_.size());
 
-  std::int32_t param_index = 0;
   for(std::int32_t sp = 0; sp < synth_part_count; sp++)
-  {
-    synth_param_bounds_.push_back(param_index);
-    for(std::int32_t p = 0; p < synth_parts[sp].info->param_count; p++, param_index++)
+    for(std::int32_t p = 0; p < synth_parts[sp].info->param_count; p++)
       synth_params_.push_back({ &synth_parts[sp], &synth_parts[sp].info->params[p] });
-  }
   synth_params = synth_params_.data();
-  synth_param_bounds = synth_param_bounds_.data();
   synth_param_count = static_cast<std::int32_t>(synth_params_.size());
 }
 
--- src/svn/svn/support/topo_rt.hpp
+++ src/svn/svn/support/topo_rt.hpp
@@ -17,13 +17,12 @@ struct synth_param
   struct param_info const* info;
 };
 
-extern std::int32_t synth_part_count;
 extern synth_part const* synth_parts;
-extern std::int32_t const* synth_part_bounds;
-
-extern std::int32_t synth_param_count;
 extern synth_param const* synth_params;
-extern std::int32_t const* synth_param_bounds;
+
+extern std::int32_t synth_part_count;
+extern std::int32_t synth_param_count;
+extern std::int32_t const* const* synth_bounds;
 
 } // namespace svn
 #endif // SVN_SUPPORT_TOPO_RT_HPP
