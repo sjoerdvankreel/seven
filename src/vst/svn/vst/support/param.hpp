401b838f91dec07c8b8ff7790ee81b65987306f0
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-22 18:25:07 +0200

    123

--- src/vst/svn/vst/support/param.hpp
+++ src/vst/svn/vst/support/param.hpp
@@ -2,11 +2,14 @@
 #define SVN_VST_SUPPORT_PARAM_HPP
 
 #include <svn/support/topo_rt.hpp>
+#include <svn/support/topo_static.hpp>
+
+#include <cmath>
 #include <cstdint>
 
 namespace Svn::Vst {
 
-std::int32_t
+inline std::int32_t
 paramStepCount(std::int32_t param)
 {
   auto const& info = *svn::synth_params[param].info;
@@ -14,23 +17,28 @@ paramStepCount(std::int32_t param)
   return info.max.discrete - info.min.discrete;
 }
 
-double
+inline std::int32_t
+paramDenormalizeDiscrete(std::int32_t param, double val)
+{
+  auto const& info = *svn::synth_params[param].info;
+  std::int32_t stepCount = paramStepCount(param);
+  return std::min(stepCount, static_cast<std::int32_t>(val * (stepCount + 1)));
+}
+
+inline std::int32_t
+paramNormalizeDiscrete(std::int32_t param, std::int32_t val)
+{
+  auto const& info = *svn::synth_params[param].info;
+  return (val - info.min.discrete) / static_cast<double>(paramStepCount(param));
+}
+
+inline double
 paramNormalizeDefault(std::int32_t param)
 {
   auto const& info = *svn::synth_params[param].info;
   if (info.type == svn::param_type::real) return static_cast<double>(info.default_.real);
-  return info.max.discrete - info.min.discrete;
+  return paramNormalizeDiscrete(param, info.default_.discrete);
 }
-
-double
-paramNormalizeReal(std::int32_t param, float val);
-float
-paramDenormalizeReal(std::int32_t param, double val);
-
-std::int32_t
-paramNormalizeDiscrete(std::int32_t param, std::int32_t val);
-double
-paramDenormalizeDiscrete(std::int32_t param, double val);
 
 } // namespace Svn::Vst
 #endif // SVN_VST_SUPPORT_PARAM_HPP
