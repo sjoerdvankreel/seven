6577f50cb65202fba3c5798a081b0cb53032deff
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-05-26 20:29:48 +0200

    172

--- src/svn.synth/svn.synth/topology.cpp
+++ src/svn.synth/svn.synth/topology.cpp
@@ -1,48 +1,51 @@
-#include <svn/support/topo_static.hpp>
-#include <cassert>
+#include <svn.synth/topology.hpp>
+#include <svn.base/static/item_name.hpp>
 
-namespace svn {
-static item_info const
+using namespace svn::base;
+
+namespace svn::synth {
+
+static item_name const
 filter_types[filter_type::count] =
 {
   { L"Lddr", L"Ladder" },
   { L"StVar", L"State variable" }
 };
 
-static item_info const
-unit_types[unit_type::count] =
+static item_name const
+oscillator_types[oscillator_type::count] =
 {
   { L"Blep", L"Bandlimited step" },
   { L"Blamp", L"Bandlimited ramp" },
   { L"Dsf", L"Discrete summation formulae" }
 };
 
-part_info const
-part_infos[] =
+part_descriptor const
+part_descriptors[] =
 {
-  { { L"Unit", L"Unit generator" }, unit_count, unit_params, unit_param::count },
-  { { L"Filter", L"Filter" }, filter_count, filter_params, filter_param::count }
+  { part_type::oscillator, { L"Osc", L"Oscillator" }, oscillator_count, oscillator_params, oscillator_param::count },
+  { part_type::filter, { L"Filter", L"Filter" }, filter_count, filter_params, filter_param::count }
 };
 
-param_info const
-unit_params[unit_param::count] =
+param_descriptor const
+oscillator_params[oscillator_param::count] =
 {
   { { L"On", L"Enabled" }, false },
-  { { L"Type", L"Type" }, unit_types, unit_type::count },
-  { { L"Lvl", L"Level" }, L"dB", 1.0, bounds_linear_unit, bounds_decibel },
+  { { L"Type", L"Type" }, oscillator_types, oscillator_type::count },
+  { { L"Lvl", L"Level" }, L"dB", 1.0, param_bounds::linear_unit(), param_bounds::decibel() },
   { { L"Dtn", L"Detune" }, L"Cent", 0, -50, 50 },
-  { { L"Dt2", L"Detune2" }, L"Cent", 0.5, bounds_linear(-0.5, 0.5), bounds_linear(-50.0, 50.0) },
-  { { L"Pan", L"Panning" }, L"%", 0.5, bounds_linear_unit, bounds_linear(-100.0, 100.0) },
-  { { L"Pw", L"Pulse width" }, L"%", 1.0, bounds_linear(0.0, 0.5), bounds_linear(0.0, 100.0) }
+  { { L"Dt2", L"Detune2" }, L"Cent", 0.5, param_bounds::linear(-0.5, 0.5), param_bounds::linear(-50.0, 50.0) },
+  { { L"Pan", L"Panning" }, L"%", 0.5, param_bounds::linear_unit(), param_bounds::linear(-100.0, 100.0) },
+  { { L"Pw", L"Pulse width" }, L"%", 1.0, param_bounds::linear(0.0, 0.5), param_bounds::linear(0.0, 100.0) }
 };
 
-param_info const
+param_descriptor const
 filter_params[filter_param::count] =
 {
   { { L"On", L"Enabled" }, false },
   { { L"Type", L"Type" }, filter_types, filter_type::count },
-  { { L"Frq", L"Frequency" }, L"Hz", 0.5, bounds_logarithmic(20.0, 20000.0, 2000.0), bounds_logarithmic(20.0, 20000.0, 2000.0) },
-  { { L"Res", L"Resonance" }, L"", 0.0, bounds_linear_unit, bounds_linear_unit }
+  { { L"Frq", L"Frequency" }, L"Hz", 0.5, param_bounds::logarithmic(20.0, 20000.0, 2000.0), param_bounds::logarithmic(20.0, 20000.0, 2000.0) },
+  { { L"Res", L"Resonance" }, L"", 0.0, param_bounds::linear_unit(), param_bounds::linear_unit() }
 };
 
-} // namespace svn
+} // namespace svn::synth
--- src/svn.synth/svn.synth/topology.hpp
+++ src/svn.synth/svn.synth/topology.hpp
@@ -1,32 +1,29 @@
-#ifndef SVN_SUPPORT_TOPO_STATIC_HPP
-#define SVN_SUPPORT_TOPO_STATIC_HPP
+#ifndef SVN_SYNTH_TOPOLOGY_HPP
+#define SVN_SYNTH_TOPOLOGY_HPP
 
-#include <svn/support/param_value.hpp>
-#include <cmath>
-#include <limits>
-#include <cstdint>
+#include <svn.base/static/part_descriptor.hpp>
+#include <svn.base/static/param_descriptor.hpp>
 
-namespace svn {
+namespace svn::synth {
 
-extern part_info const part_infos[];
-extern param_info const unit_params[];
-extern param_info const filter_params[];
-
-inline std::int32_t constexpr unit_count = 2;
 inline std::int32_t constexpr filter_count = 1;
+inline std::int32_t constexpr oscillator_count = 2;
 
-struct part_type_t { enum value { unit, filter, count }; };
-typedef part_type_t::value part_type;
+extern base::part_descriptor const part_descriptors[];
+extern base::param_descriptor const filter_params[];
+extern base::param_descriptor const oscillator_params[];
 
-struct unit_type_t { enum value { blep, blamp, dsf, count }; };
+struct part_type_t { enum value { oscillator, filter, count }; };
+struct oscillator_type_t { enum value { blep, blamp, dsf, count }; };
 struct filter_type_t { enum value { ladder, state_variable, count }; };
-typedef unit_type_t::value unit_type;
-typedef filter_type_t::value filter_type;
-
 struct filter_param_t { enum value { on, type, frequency, resonance, count }; };
-struct unit_param_t { enum value { on, type, level, detune, detune2, panning, pw, count }; };
-typedef unit_param_t::value unit_param;
-typedef filter_param_t::value filter_param;
+struct oscillator_param_t { enum value { on, type, level, detune, detune2, panning, pw, count }; };
 
-} // namespace svn
-#endif // SVN_SUPPORT_TOPO_STATIC_HPP
+typedef part_type_t::value part_type;
+typedef filter_type_t::value filter_type;
+typedef filter_param_t::value filter_param;
+typedef oscillator_type_t::value oscillator_type;
+typedef oscillator_param_t::value oscillator_param;
+
+} // namespace svn::synth
+#endif // SVN_SYNTH_TOPOLOGY_HPP
