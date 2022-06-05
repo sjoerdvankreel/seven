4b1f6a66bda6f8d9c17385676f547418334e7a3e
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-06-05 16:01:13 +0200

    262

--- src/svn.synth/svn.synth/dsp/voice_oscillator.cpp
+++ src/svn.synth/svn.synth/dsp/voice_oscillator.cpp
@@ -11,6 +11,33 @@ using namespace svn::base;
 
 namespace svn::synth {
 
+static inline float
+osc_sine(float phase)
+{
+  assert(0.0f <= phase && phase < 1.0f);
+  return std::sin(2.0f * std::numbers::pi * phase);
+}
+
+static inline float
+osc_blep(float sample_rate, float frequency, float phase)
+{
+  float saw = 2.0f * phase - 1.0f;
+  float increment = frequency / sample_rate;
+  assert(0.0f <= phase && phase < 1.0f);
+
+  if (phase < increment)
+  {
+    float blep = phase / increment;
+    return saw - ((2.0f - blep) * blep - 1.0f);
+  }
+  if (phase >= 1.0f - increment)
+  {
+    float blep = (phase - 1.0f) / increment;
+    return saw - ((blep + 2.0f) * blep + 1.0f);
+  }
+  return saw;
+}
+
 voice_oscillator::
 voice_oscillator(
   float sample_rate, std::int32_t midi_note):
@@ -29,13 +56,24 @@ voice_oscillator::process_block(
   for (std::int32_t s = 0; s < input.sample_count; s++)
   {
     audio[s] = 0.0f;
-    if(input.automation.get(voice_osc_param::on, s).discrete == 0) continue;
+    bool on = input.automation.get(voice_osc_param::on, s).discrete != 0;
+    if(!on) continue;
+
     float panning = input.automation.get(voice_osc_param::pan, s).real;
     float cent = input.automation.get(voice_osc_param::detune, s).real;
+    std::int32_t type = input.automation.get(voice_osc_param::type, s).discrete;
     std::int32_t note = input.automation.get(voice_osc_param::note, s).discrete;
     std::int32_t octave = input.automation.get(voice_osc_param::oct, s).discrete;
+
+    float sample;
     float frequency = note_to_frequency(12 * (octave + 1) + note + cent + _midi_note - 60);
-    float sample = std::sin(2.0f * std::numbers::pi * _phase);
+    switch (type)
+    {
+    case voice_osc_type::sine: sample = osc_sine(_phase); break;
+    case voice_osc_type::blep: sample = osc_blep(_sample_rate, frequency, _phase); break;
+    default: assert(false); break;
+    }
+
     audio[s].left = (1.0f - panning) * sample;
     audio[s].right = panning * sample;
     _phase += frequency / _sample_rate;
