1d2627771a78420d0c483e38d8054d20594101be
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-06-05 11:17:25 +0200

    244

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build
 cd build
-cmake ../ -DVST_SDK_374_SRC_DIR=C:/repos/VST_SDK/vst3sdk -DVST_SDK_374_BUILD_DIR=C:/repos/VST_SDK/vst3sdk/build
+cmake ../ -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DIR=C:/repos/vst-sdk_3.7.5/VST_SDK/vst3sdk/build
 cd ..
 set /p wait=Press return...
