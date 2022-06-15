5533b70c43f0fe3640fd1c7f6aa7444c87d025c2
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-06-15 19:03:14 +0200

    423

--- src/svn.vst.base/svn.vst.base/sdk/parameter.cpp
+++ src/svn.vst.base/svn.vst.base/sdk/parameter.cpp
@@ -1,5 +1,4 @@
 #include <svn.vst.base/sdk/parameter.hpp>
-#include <svn.base/topology/topology_info.hpp>
 #include <cassert>
 
 using namespace svn::base;
@@ -38,43 +37,20 @@ param_flags(param_type type, bool output)
 }  
 
 parameter::
-parameter(std::int32_t index, svn::base::part_info const* part, svn::base::param_info const* param) :
+parameter(std::int32_t index, 
+  svn::base::part_info const* part, 
+  svn::base::param_info const* param) :
   Parameter(
     param->runtime_name.c_str(), index, param->descriptor->unit,
     param_default_to_vst_normalized(*param->descriptor),
     param_step_count(*param->descriptor),
     param_flags(param->descriptor->type, part->descriptor->output),
-    param->part_index + 1,
-    param->descriptor->static_name.short_),
+    param->part_index + 1, param->descriptor->static_name.short_),
   _descriptor(param->descriptor)
 {    
   assert(index >= 0);
   assert(param != nullptr);
   setPrecision(param->descriptor->real.precision);
-}   
-
-ParamValue
-parameter::toPlain(ParamValue normalized) const
-{
-  switch (_descriptor->type)
-  {
-  case param_type::real: 
-    return _descriptor->real.display.to_range(normalized);
-  default:
-    return vst_normalized_to_discrete(*_descriptor, normalized);
-  }
-}
-
-ParamValue 
-parameter::toNormalized(ParamValue plain) const
-{
-  switch (_descriptor->type)
-  {
-  case param_type::real:
-    return _descriptor->real.display.from_range(plain);
-  default:
-    return discrete_to_vst_normalized(*_descriptor, static_cast<std::int32_t>(plain));
-  }
 }
 
 void
@@ -100,6 +76,26 @@ parameter::fromString(TChar const* string, ParamValue& normalized) const
   default: normalized = discrete_to_vst_normalized(*_descriptor, value.discrete); break;
   }
   return true;
+}
+
+ParamValue
+parameter::toPlain(ParamValue normalized) const
+{
+  switch (_descriptor->type)
+  {
+  case param_type::real: return _descriptor->real.display.to_range(normalized);
+  default: return vst_normalized_to_discrete(*_descriptor, normalized);
+  }
+}
+
+ParamValue
+parameter::toNormalized(ParamValue plain) const
+{
+  switch (_descriptor->type)
+  {
+  case param_type::real: return _descriptor->real.display.from_range(plain);
+  default: return discrete_to_vst_normalized(*_descriptor, static_cast<std::int32_t>(plain));
+  }
 }
 
 } // namespace svn::vst::base
--- src/svn.vst.base/svn.vst.base/sdk/parameter.hpp
+++ src/svn.vst.base/svn.vst.base/sdk/parameter.hpp
@@ -1,14 +1,15 @@
 #ifndef SVN_VST_BASE_SDK_PARAMETER_HPP
 #define SVN_VST_BASE_SDK_PARAMETER_HPP
 
-#include <public.sdk/source/vst/vstparameters.h>
 #include <svn.base/topology/topology_info.hpp>
+#include <public.sdk/source/vst/vstparameters.h>
 
 #include <cstdint>
 #include <algorithm>
 
 namespace svn::vst::base {
 
+// Vst parameter based on param_descriptor.
 class parameter:
 public Steinberg::Vst::Parameter 
 {
@@ -19,20 +20,18 @@ private:
   svn::base::param_descriptor const* const _descriptor;
 
 public:
-
-  parameter(
-    std::int32_t index, 
+  parameter(std::int32_t index, 
     svn::base::part_info const* part,
     svn::base::param_info const* param);
-  static std::int32_t vst_normalized_to_discrete(
-    svn::base::param_descriptor const& param, double val);
-  static double discrete_to_vst_normalized(
-    svn::base::param_descriptor const& param, std::int32_t val);
-
+  
+  // Forwarding to param_descriptor.
   ParamValue toPlain(ParamValue normalized) const override;
   ParamValue toNormalized(ParamValue plain) const override;
   void toString(ParamValue normalized, String128 string) const override;
   bool fromString(TChar const* string, ParamValue& normalized) const override;
+
+  static std::int32_t vst_normalized_to_discrete(svn::base::param_descriptor const& param, double val);
+  static double discrete_to_vst_normalized(svn::base::param_descriptor const& param, std::int32_t val);
 };
 
 inline double 
