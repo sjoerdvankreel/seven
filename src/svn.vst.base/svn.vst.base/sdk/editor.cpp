00ef5ecf44bb6e0a06b1586bc2ab9ada0142776b
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-06-19 20:35:49 +0200

    478 graph

--- src/svn.vst.base/svn.vst.base/sdk/editor.cpp
+++ src/svn.vst.base/svn.vst.base/sdk/editor.cpp
@@ -23,19 +23,19 @@ editor::onViewAdded(CFrame* frame, CView* view)
 {
   VST3Editor::onViewAdded(frame, view);
   CControl* control = dynamic_cast<CControl*>(view);
-  if(control == nullptr || control->getTag() < 0) return;
-  _controls[control->getTag()].push_back(control);
+  if(control != nullptr && control->getTag() >= 0)
+    _controls[control->getTag()].push_back(control);
 }
  
 void 
 editor::onViewRemoved(CFrame* frame, CView* view)
 {
   CControl* control = dynamic_cast<CControl*>(view);
-  if (control == nullptr || control->getTag() < 0) return;
-  for(std::size_t param = 0; param < _controls.size(); param++)
-    for (std::size_t control = 0; control < _controls[param].size(); control++)
-      if(_controls[param][control] == view)
-        _controls[param].erase(_controls[param].begin() + control);
+  if (control != nullptr && control->getTag() >= 0)
+    for(std::size_t param = 0; param < _controls.size(); param++)
+      for (std::size_t control = 0; control < _controls[param].size(); control++)
+        if(_controls[param][control] == view)
+          _controls[param].erase(_controls[param].begin() + control);
   VST3Editor::onViewRemoved(frame, view);
 }
 
