116b5475e2ebe2d6ff21c6c2deaebff0c7f30575
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-06-19 21:59:27 +0200

    483 graph

--- src/svn.synth/svn.synth/dsp/oscillator.cpp
+++ src/svn.synth/svn.synth/dsp/oscillator.cpp
@@ -10,8 +10,8 @@ using namespace svn::base;
 
 namespace svn::synth {
     
-static inline float
-oscillator_blep(float sample_rate, float frequency, float phase)
+static float
+oscillator_blep_saw(float sample_rate, float frequency, float phase)
 {
   float saw = 2.0f * phase - 1.0f;
   float increment = frequency / sample_rate;
@@ -29,11 +29,38 @@ oscillator_blep(float sample_rate, float frequency, float phase)
   }
   return saw;
 }
+
+static float
+oscillator_blep(std::int32_t blep_type, float sample_rate, float frequency, float phase)
+{
+  float saw1;
+  float saw2;
+  float phase2;
+
+  switch (blep_type)
+  {
+  case oscillator_blep_type::triangle:
+    return 0.0f;
+  case oscillator_blep_type::saw:
+    return oscillator_blep_saw(sample_rate, frequency, phase);
+  case oscillator_blep_type::pulse:
+    phase2 = phase + 0.5f;
+    phase2 -= std::floor(phase2);
+    saw1 = oscillator_blep_saw(sample_rate, frequency, phase);
+    saw2 = oscillator_blep_saw(sample_rate, frequency, phase2);
+    return (saw1 - saw2) * 0.5f;
+  default:
+    assert(false);
+    return 0.0f;
+  }
+}
   
 void
 oscillator::process_block(voice_input const& input, audio_sample* audio)
 {
+  std::int32_t blep_type;
   assert(audio != nullptr);
+
   for (std::int32_t s = 0; s < input.sample_count; s++)
   {
     audio[s] = 0.0f;
@@ -51,10 +78,19 @@ oscillator::process_block(voice_input const& input, audio_sample* audio)
     float frequency = note_to_frequency(12 * (octave + 1) + note + cent + _midi_note - 60);
     switch (type)
     {
-    case oscillator_type::blmp: sample = 0.0f; break;
-    case oscillator_type::sine: sample = std::sin(2.0f * std::numbers::pi * _phase); break;
-    case oscillator_type::blep: sample = oscillator_blep(_sample_rate, frequency, _phase); break;
-    default: assert(false); break;
+    case oscillator_type::blmp: 
+      sample = 0.0f; 
+      break;
+    case oscillator_type::sine: 
+      sample = std::sin(2.0f * std::numbers::pi * _phase); 
+      break;
+    case oscillator_type::blep: 
+      blep_type = input.automation.get(oscillator_param::blep_type, s).discrete;
+      sample = oscillator_blep(blep_type, _sample_rate, frequency, _phase); 
+      break;
+    default: 
+      assert(false); 
+      break;
     } 
      
     audio[s].left = (1.0f - panning) * sample * amp;
