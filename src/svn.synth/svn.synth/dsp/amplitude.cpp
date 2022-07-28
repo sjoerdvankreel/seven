e07dc4f25dd6c5eae0aae0a26059f85f96b2147d
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-28 21:17:25 +0200

    804

--- src/svn.synth/svn.synth/dsp/amplitude.cpp
+++ src/svn.synth/svn.synth/dsp/amplitude.cpp
@@ -14,16 +14,16 @@ amplitude::process_block(
 {
   float const* const* transformed_cv;
   automation_view automation(input.automation.rearrange_params(part_type::amplitude, 0));
-  cv_time += cv.transform_modulated(input, automation, cv_route_output::amp, 0, cv_route_amp_mapping, transformed_cv);
+  cv_time += cv.transform_modulated(input, automation, cv_route_output::amp, 0, cv_route_amp_mapping, transformed_cv);  
   double start_time = performance_counter();
+  float const* level = transformed_cv[amplitude_param::level];
+  float const* panning = transformed_cv[amplitude_param::pan];
   for (std::int32_t s = 0; s < input.sample_count; s++)
   { 
-    float level = automation.get_transformed_dsp(amplitude_param::level, s, transformed_cv);
-    float panning = automation.get_transformed_dsp(amplitude_param::pan, s, transformed_cv);
     float env1_uni = !env1_cv[s].bipolar? env1_cv[s].value: (env1_cv[s].value + 1.0f) * 0.5f;
-    float final_amp = level * env1_uni;
-    float left = audio_in[s].left * final_amp * (1.0f - panning);
-    float right = audio_in[s].right * final_amp * panning;
+    float final_amp = level[s] * env1_uni;
+    float left = audio_in[s].left * final_amp * (1.0f - panning[s]);
+    float right = audio_in[s].right * final_amp * panning[s];
     audio_out[s] = sanity_audio(audio_sample32({left, right}));
   }
   return performance_counter() - start_time;
