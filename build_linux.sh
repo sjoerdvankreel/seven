953f92b423affde7f1c0d9832f8c6470e29e8e73
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-31 14:29:17 +0200

    816

--- build_linux.sh
+++ build_linux.sh
@@ -5,6 +5,6 @@ mkdir -p build
 cd build
 mkdir -p linux
 cd linux
-cmake ../../ -DVST_SDK_375_SRC_DIR=/media/sf_vst-sdk_3.7.5/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DIR=/media/sf_vst-sdk_3.7.5/VST_SDK/vst3sdk/build
+cmake ../../ -DVST_SDK_375_SRC_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/debug/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/release/lib/Release
 cd ..
 cd ..
