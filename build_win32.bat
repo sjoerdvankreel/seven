f74f7d18d8f738934fa31d22d26596413a9f2620
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-31 14:11:13 +0200

    815

--- build_win32.bat
+++ build_win32.bat
@@ -3,7 +3,7 @@ mkdir build
 cd build
 mkdir win32
 cd win32
-cmake ../../ -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/VST_SDK/vst3sdk/build/lib/Release
+cmake ../../ -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Release
 cd ..
 cd ..
 set /p wait=Press return...
