a039f2c0ca1113f2013c3e4260670ee98a512dc5
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-31 16:06:40 +0200

    820

--- build_win32.bat
+++ build_win32.bat
@@ -6,12 +6,12 @@ cd win32
 
 mkdir debug
 cd debug
-cmake ../../../ -DCMAKE_BUILD_TYPE=Debug -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Release
+cmake ../../../ -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Release
 cd ..
 
 mkdir release
 cd release
-cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Release
+cmake ../../../ -DVST_SDK_375_SRC_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.5/win32/VST_SDK/vst3sdk/build/lib/Release
 cd ..
 
 cd ..
