eadbcbef4647df10d5abbaf9020e5ad034cb1a7d
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-31 16:21:18 +0200

    823

--- build_linux.sh
+++ build_linux.sh
@@ -9,11 +9,13 @@ cd linux
 mkdir -p debug
 cd debug
 cmake ../../../ -DCMAKE_BUILD_TYPE=Debug -DVST_SDK_375_SRC_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/debug/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/release/lib/Release
+make
 cd ..
 
 mkdir -p release
-cmake ../../../ -DCMAKE_BUILD_TYPE=Debug -DVST_SDK_375_SRC_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/debug/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/release/lib/Release
 cd release
+cmake ../../../ -DCMAKE_BUILD_TYPE=Debug -DVST_SDK_375_SRC_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk -DVST_SDK_375_BUILD_DEBUG_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/debug/lib/Debug -DVST_SDK_375_BUILD_RELEASE_DIR=/media/sf_vst-sdk_3.7.5/linux/VST_SDK/vst3sdk/build/release/lib/Release
+make
 cd ..
 
 cd ..
