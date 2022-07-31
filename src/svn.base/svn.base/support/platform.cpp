b5aff9b41150df6c0a5467ee144d35a977ed685d
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-07-31 16:59:14 +0200

    830

--- src/svn.base/svn.base/support/platform.cpp
+++ src/svn.base/svn.base/support/platform.cpp
@@ -3,6 +3,8 @@
 #if WIN32
 #define NOMINMAX 1
 #include <Windows.h>
+#else
+#include <time.h>
 #endif
 
 #include <cassert>
@@ -12,6 +14,7 @@ namespace svn::base {
 double
 performance_counter()
 {
+#if WIN32
   bool ok;
   LARGE_INTEGER counter;
   LARGE_INTEGER frequency;
@@ -20,6 +23,11 @@ performance_counter()
   ok = QueryPerformanceFrequency(&frequency);
   assert(ok);
   return static_cast<double>(counter.QuadPart) / static_cast<double>(frequency.QuadPart);
+#else
+  struct timespec tv;
+  clock_gettime(CLOCK_REALTIME, &tv);
+  return static_cast<double>(tv.tv_sec) + tv.tv_nsec / 1000000000.0;
+#endif
 }
  
 } // namespace svn::base
